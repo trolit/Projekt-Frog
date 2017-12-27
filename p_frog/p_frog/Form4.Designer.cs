d7685e97ca695dcaea11a8bcafbc4f1fd123adc6
Idziku <iddziku@gmail.com>
2017-12-27 13:31:12 +0100

    Zapomnialem o dodaniu Form4 do obserwowanych

---
+++ p_frog/p_frog/Form4.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace p_frog
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.button1 = new System.Windows.Forms.Button();
+            this.level_1 = new System.Windows.Forms.Button();
+            this.level_2 = new System.Windows.Forms.Button();
+            this.level_3 = new System.Windows.Forms.Button();
+            this.level_4 = new System.Windows.Forms.Button();
+            this.level_5 = new System.Windows.Forms.Button();
+            this.level_6 = new System.Windows.Forms.Button();
+            this.level_7 = new System.Windows.Forms.Button();
+            this.level_8 = new System.Windows.Forms.Button();
+            this.level_9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SandyBrown;
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "powrót do menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // level_1
+            // 
+            this.level_1.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_1.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_1.Location = new System.Drawing.Point(13, 70);
+            this.level_1.Name = "level_1";
+            this.level_1.Size = new System.Drawing.Size(63, 55);
+            this.level_1.TabIndex = 1;
+            this.level_1.Text = "1";
+            this.level_1.UseVisualStyleBackColor = false;
+            // 
+            // level_2
+            // 
+            this.level_2.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_2.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_2.Location = new System.Drawing.Point(82, 70);
+            this.level_2.Name = "level_2";
+            this.level_2.Size = new System.Drawing.Size(63, 55);
+            this.level_2.TabIndex = 2;
+            this.level_2.Text = "2";
+            this.level_2.UseVisualStyleBackColor = false;
+            // 
+            // level_3
+            // 
+            this.level_3.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_3.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_3.Location = new System.Drawing.Point(151, 70);
+            this.level_3.Name = "level_3";
+            this.level_3.Size = new System.Drawing.Size(63, 55);
+            this.level_3.TabIndex = 3;
+            this.level_3.Text = "3";
+            this.level_3.UseVisualStyleBackColor = false;
+            // 
+            // level_4
+            // 
+            this.level_4.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_4.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_4.Location = new System.Drawing.Point(220, 70);
+            this.level_4.Name = "level_4";
+            this.level_4.Size = new System.Drawing.Size(63, 55);
+            this.level_4.TabIndex = 4;
+            this.level_4.Text = "4";
+            this.level_4.UseVisualStyleBackColor = false;
+            // 
+            // level_5
+            // 
+            this.level_5.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_5.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_5.Location = new System.Drawing.Point(289, 70);
+            this.level_5.Name = "level_5";
+            this.level_5.Size = new System.Drawing.Size(63, 55);
+            this.level_5.TabIndex = 5;
+            this.level_5.Text = "5";
+            this.level_5.UseVisualStyleBackColor = false;
+            // 
+            // level_6
+            // 
+            this.level_6.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_6.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_6.Location = new System.Drawing.Point(358, 70);
+            this.level_6.Name = "level_6";
+            this.level_6.Size = new System.Drawing.Size(63, 55);
+            this.level_6.TabIndex = 6;
+            this.level_6.Text = "6";
+            this.level_6.UseVisualStyleBackColor = false;
+            // 
+            // level_7
+            // 
+            this.level_7.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_7.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_7.Location = new System.Drawing.Point(13, 144);
+            this.level_7.Name = "level_7";
+            this.level_7.Size = new System.Drawing.Size(63, 55);
+            this.level_7.TabIndex = 7;
+            this.level_7.Text = "7";
+            this.level_7.UseVisualStyleBackColor = false;
+            // 
+            // level_8
+            // 
+            this.level_8.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_8.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_8.Location = new System.Drawing.Point(82, 144);
+            this.level_8.Name = "level_8";
+            this.level_8.Size = new System.Drawing.Size(63, 55);
+            this.level_8.TabIndex = 8;
+            this.level_8.Text = "8";
+            this.level_8.UseVisualStyleBackColor = false;
+            // 
+            // level_9
+            // 
+            this.level_9.BackColor = System.Drawing.Color.Chartreuse;
+            this.level_9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.level_9.Font = new System.Drawing.Font("Lucida Console", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level_9.Location = new System.Drawing.Point(151, 144);
+            this.level_9.Name = "level_9";
+            this.level_9.Size = new System.Drawing.Size(63, 55);
+            this.level_9.TabIndex = 9;
+            this.level_9.Text = "9";
+            this.level_9.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(146, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 27);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Wybierz poziom:";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(436, 379);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.level_9);
+            this.Controls.Add(this.level_8);
+            this.Controls.Add(this.level_7);
+            this.Controls.Add(this.level_6);
+            this.Controls.Add(this.level_5);
+            this.Controls.Add(this.level_4);
+            this.Controls.Add(this.level_3);
+            this.Controls.Add(this.level_2);
+            this.Controls.Add(this.level_1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projekt Frog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button level_1;
+        private System.Windows.Forms.Button level_2;
+        private System.Windows.Forms.Button level_3;
+        private System.Windows.Forms.Button level_4;
+        private System.Windows.Forms.Button level_5;
+        private System.Windows.Forms.Button level_6;
+        private System.Windows.Forms.Button level_7;
+        private System.Windows.Forms.Button level_8;
+        private System.Windows.Forms.Button level_9;
+        private System.Windows.Forms.Label label1;
+    }
+}
